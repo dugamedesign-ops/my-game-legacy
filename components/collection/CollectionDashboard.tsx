02eccafb2d6a542728a0074c550454aba257b6b0
dugamedesign-ops <dugamedesign@gmail.com>
2026-05-01 16:40:15 -0300

    Enable context-menu delete on latest added carousel cards

--- components/collection/CollectionDashboard.tsx
+++ components/collection/CollectionDashboard.tsx
@@ -1355,6 +1355,9 @@ export function CollectionDashboard({ items }: CollectionDashboardProps) {
                         item={item}
                         size="small"
                         onClick={setSelectedItem}
+                        onContextMenu={(selectedItem, x, y) => {
+                          setContextMenu({ item: selectedItem, x, y });
+                        }}
                         showMediaSeals={false}
                       />
                     </div>
